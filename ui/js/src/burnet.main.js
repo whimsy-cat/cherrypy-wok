kimchi-project/kimchi

commit cc89b1e3eb53b2e05813e6e4fe1e5ceee517b8bf

add js globalization method
In order to realize js globalization our goals are as follows:

 1. Messages need to be in po format for translation consistency.

 2. Construction should be done at runtime

base on the two rules as above, and html can use getText to
realize it. in order to realize number 1 goal, we have to put
the js message which needs to be translated into htmls.

so we add a new file called i18n.html.tmpl.

this file is an empty file which only add the script as follows:

'burnet.list.guest.fail.msg':
$dq$_('burnet.list.guest.fail.msg')$dq,

'burnet.list.guest.fail.msg' is the key in js files , which js use
 this key like this : i18n[burnet.list.guest.fail.msg]

 $dq$_('burnet.list.guest.fail.msg')$dq} is the value , it will be
 replaced by text of different languages in the runtime when the
server generate the html.

also the i18n.html.tmpl can be auto generated by a tool in the
build time.
Signed-off-by: zhoumeina <zhoumein@linux.vnet.ibm.com>
Signed-off-by: ShaoHe Feng <shaohef@linux.vnet.ibm.com>

ui/js/src/burnet.main.js
@@ -38,5 +38,6 @@ burnet.main = function() {
 	var width = currentMenu.parent().width();
 	$('.menu-arrow').css('left', left+width/2-10);
 
+	$('#main').load('i18n.html');
 	$('#main').load('guestTab.html');
 };

i18n/po/en_US/LC_MESSAGES/burnet.po
@@ -20,7 +20,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: burnet 0.1\n"
 "POT-Creation-Date: 2013-04-18 22:35+CST\n"
-"PO-Revision-Date: 2013-06-27 10:48+0000\n"
+"PO-Revision-Date: 2013-07-19 03:01+0000\n"
 "Last-Translator: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "Language-Team: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "MIME-Version: 1.0\n"
@@ -56,11 +56,11 @@ msgstr ""
 msgid "Language:"
 msgstr ""
 
-#: ui/pages/burnet-ui.html.tmpl:86 ui/pages/dev-ui.html.tmpl:75
+#: ui/pages/burnet-ui.html.tmpl:87 ui/pages/dev-ui.html.tmpl:75
 msgid "Guests"
 msgstr ""
 
-#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guest.html.tmpl:47
+#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guestTab.html.tmpl:49
 msgid "Storage"
 msgstr ""
 
@@ -97,7 +97,7 @@ msgid "Settings"
 msgstr ""
 
 #: ui/pages/dev-ui.html.tmpl:94 ui/pages/dev-ui.html.tmpl:105
-#: ui/pages/guest.html.tmpl:85 ui/pages/template.html.tmpl:61
+#: ui/pages/guest.html.tmpl:68 ui/pages/template.html.tmpl:61
 msgid "Delete"
 msgstr ""
 
@@ -105,7 +105,7 @@ msgstr ""
 msgid "More..."
 msgstr ""
 
-#: ui/pages/burnet-ui.html.tmpl:89 ui/pages/dev-ui.html.tmpl:102
+#: ui/pages/burnet-ui.html.tmpl:90 ui/pages/dev-ui.html.tmpl:102
 #: ui/pages/template.html.tmpl:48
 msgid "Templates"
 msgstr ""
@@ -134,24 +134,24 @@ msgstr ""
 msgid "IRC"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:44
+#: ui/pages/guestTab.html.tmpl:46
 msgid "Name"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:45 ui/pages/template-edit.html.tmpl:71
+#: ui/pages/guestTab.html.tmpl:47 ui/pages/template-edit.html.tmpl:71
 msgid "CPU"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:64 ui/pages/guest.html.tmpl:46
-#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:75
+#: ui/pages/guest-add.html.tmpl:64 ui/pages/guestTab.html.tmpl:48
+#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:78
 msgid "Memory"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:48
+#: ui/pages/guestTab.html.tmpl:50
 msgid "Livetile"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:49 ui/pages/guest.html.tmpl:82
+#: ui/pages/guest.html.tmpl:65 ui/pages/guestTab.html.tmpl:51
 #: ui/pages/template.html.tmpl:58
 msgid "Actions"
 msgstr ""
@@ -160,7 +160,7 @@ msgstr ""
 msgid "Create a New Virtual Machine"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:35 ui/pages/template-add.html.tmpl:35
+#: ui/pages/guest-add.html.tmpl:35
 msgid "Virtual Machine Name"
 msgstr ""
 
@@ -178,7 +178,7 @@ msgstr ""
 msgid "Please choose a template."
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:69
+#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:72
 msgid "OS"
 msgstr ""
 
@@ -190,8 +190,8 @@ msgstr ""
 msgid "CPUS"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:79
-#: ui/pages/template-add.html.tmpl:90
+#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:78
+#: ui/pages/template-add.html.tmpl:89
 msgid "Create"
 msgstr ""
 
@@ -203,47 +203,47 @@ msgstr ""
 msgid "Edit"
 msgstr ""
 
-#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:70
+#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:73
 msgid "Version"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:73
+#: ui/pages/template.html.tmpl:76
 msgid "Default Settings"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:74
+#: ui/pages/template.html.tmpl:77
 msgid "CPUs"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:41
+#: ui/pages/template-add.html.tmpl:40
 msgid "Add Template"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:49
+#: ui/pages/template-add.html.tmpl:48
 msgid "What would you like to do?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:53
+#: ui/pages/template-add.html.tmpl:52
 msgid "Select a specific ISO to create a template."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:60
+#: ui/pages/template-add.html.tmpl:59
 msgid "Where is the ISO file?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:64
+#: ui/pages/template-add.html.tmpl:63
 msgid "The ISO is on my server."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:68
+#: ui/pages/template-add.html.tmpl:67
 msgid "I want to stream an ISO from the Internet."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:75
+#: ui/pages/template-add.html.tmpl:74
 msgid "Select the ISO you\\342\\200\\231d like to use to create a template:"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:86
+#: ui/pages/template-add.html.tmpl:85
 msgid "Please enter a URL:"
 msgstr ""
 
@@ -306,3 +306,39 @@ msgstr ""
 #: ui/pages/template-edit.html.tmpl:138
 msgid "Save"
 msgstr ""
+
+#: ui/pages/i18n.html.tmpl:40
+msgid "temp.msg.fail.list"
+msgstr "Failed to list the template"
+
+#: ui/pages/i18n.html.tmpl:41
+msgid "temp.msg.choose.temp"
+msgstr "Please choose a template"
+
+#: ui/pages/i18n.html.tmpl:42
+msgid "vm.msg.fail.create.vm"
+msgstr "Failed to create vm"
+
+#: ui/pages/i18n.html.tmpl:43
+msgid "msg.fail.start"
+msgstr "Failed to start"
+
+#: ui/pages/i18n.html.tmpl:44
+msgid "msg.fail.stop"
+msgstr "Failed to stop"
+
+#: ui/pages/i18n.html.tmpl:45
+msgid "msg.fail.reset"
+msgstr "Failed to reset"
+
+#: ui/pages/i18n.html.tmpl:46
+msgid "msg.fail.delete"
+msgstr "Failed to delete"
+
+#: ui/pages/i18n.html.tmpl:47
+msgid "msg.fail.list.guests"
+msgstr "Failed to list guests"
+
+#: ui/pages/i18n.html.tmpl:48
+msgid "msg.fail.create.template"
+msgstr "Failed to create template"

i18n/po/pt_BR/LC_MESSAGES/burnet.po
@@ -20,7 +20,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: burnet 0.1\n"
 "POT-Creation-Date: 2013-04-18 22:35+CST\n"
-"PO-Revision-Date: 2013-06-27 10:48+0000\n"
+"PO-Revision-Date: 2013-07-19 03:01+0000\n"
 "Last-Translator: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "Language-Team: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "MIME-Version: 1.0\n"
@@ -56,11 +56,11 @@ msgstr ""
 msgid "Language:"
 msgstr ""
 
-#: ui/pages/burnet-ui.html.tmpl:86 ui/pages/dev-ui.html.tmpl:75
+#: ui/pages/burnet-ui.html.tmpl:87 ui/pages/dev-ui.html.tmpl:75
 msgid "Guests"
 msgstr ""
 
-#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guest.html.tmpl:47
+#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guestTab.html.tmpl:49
 msgid "Storage"
 msgstr ""
 
@@ -97,7 +97,7 @@ msgid "Settings"
 msgstr ""
 
 #: ui/pages/dev-ui.html.tmpl:94 ui/pages/dev-ui.html.tmpl:105
-#: ui/pages/guest.html.tmpl:85 ui/pages/template.html.tmpl:61
+#: ui/pages/guest.html.tmpl:68 ui/pages/template.html.tmpl:61
 msgid "Delete"
 msgstr ""
 
@@ -105,7 +105,7 @@ msgstr ""
 msgid "More..."
 msgstr ""
 
-#: ui/pages/burnet-ui.html.tmpl:89 ui/pages/dev-ui.html.tmpl:102
+#: ui/pages/burnet-ui.html.tmpl:90 ui/pages/dev-ui.html.tmpl:102
 #: ui/pages/template.html.tmpl:48
 msgid "Templates"
 msgstr ""
@@ -134,24 +134,24 @@ msgstr ""
 msgid "IRC"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:44
+#: ui/pages/guestTab.html.tmpl:46
 msgid "Name"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:45 ui/pages/template-edit.html.tmpl:71
+#: ui/pages/guestTab.html.tmpl:47 ui/pages/template-edit.html.tmpl:71
 msgid "CPU"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:64 ui/pages/guest.html.tmpl:46
-#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:75
+#: ui/pages/guest-add.html.tmpl:64 ui/pages/guestTab.html.tmpl:48
+#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:78
 msgid "Memory"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:48
+#: ui/pages/guestTab.html.tmpl:50
 msgid "Livetile"
 msgstr ""
 
-#: ui/pages/guest.html.tmpl:49 ui/pages/guest.html.tmpl:82
+#: ui/pages/guest.html.tmpl:65 ui/pages/guestTab.html.tmpl:51
 #: ui/pages/template.html.tmpl:58
 msgid "Actions"
 msgstr ""
@@ -160,7 +160,7 @@ msgstr ""
 msgid "Create a New Virtual Machine"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:35 ui/pages/template-add.html.tmpl:35
+#: ui/pages/guest-add.html.tmpl:35
 msgid "Virtual Machine Name"
 msgstr ""
 
@@ -178,7 +178,7 @@ msgstr ""
 msgid "Please choose a template."
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:69
+#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:72
 msgid "OS"
 msgstr ""
 
@@ -190,8 +190,8 @@ msgstr ""
 msgid "CPUS"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:79
-#: ui/pages/template-add.html.tmpl:90
+#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:78
+#: ui/pages/template-add.html.tmpl:89
 msgid "Create"
 msgstr ""
 
@@ -203,47 +203,47 @@ msgstr ""
 msgid "Edit"
 msgstr ""
 
-#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:70
+#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:73
 msgid "Version"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:73
+#: ui/pages/template.html.tmpl:76
 msgid "Default Settings"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:74
+#: ui/pages/template.html.tmpl:77
 msgid "CPUs"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:41
+#: ui/pages/template-add.html.tmpl:40
 msgid "Add Template"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:49
+#: ui/pages/template-add.html.tmpl:48
 msgid "What would you like to do?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:53
+#: ui/pages/template-add.html.tmpl:52
 msgid "Select a specific ISO to create a template."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:60
+#: ui/pages/template-add.html.tmpl:59
 msgid "Where is the ISO file?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:64
+#: ui/pages/template-add.html.tmpl:63
 msgid "The ISO is on my server."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:68
+#: ui/pages/template-add.html.tmpl:67
 msgid "I want to stream an ISO from the Internet."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:75
+#: ui/pages/template-add.html.tmpl:74
 msgid "Select the ISO you\\342\\200\\231d like to use to create a template:"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:86
+#: ui/pages/template-add.html.tmpl:85
 msgid "Please enter a URL:"
 msgstr ""
 
@@ -305,4 +305,40 @@ msgstr ""
 
 #: ui/pages/template-edit.html.tmpl:138
 msgid "Save"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:40
+msgid "temp.msg.fail.list"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:41
+msgid "temp.msg.choose.temp"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:42
+msgid "vm.msg.fail.create.vm"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:43
+msgid "msg.fail.start"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:44
+msgid "msg.fail.stop"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:45
+msgid "msg.fail.reset"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:46
+msgid "msg.fail.delete"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:47
+msgid "msg.fail.list.guests"
+msgstr ""
+
+#: ui/pages/i18n.html.tmpl:48
+msgid "msg.fail.create.template"
 msgstr ""

i18n/po/zh_CN/LC_MESSAGES/burnet.po
@@ -20,7 +20,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: burnet 0.1\n"
 "POT-Creation-Date: 2013-04-18 22:35+CST\n"
-"PO-Revision-Date: 2013-06-27 10:48+0000\n"
+"PO-Revision-Date: 2013-07-19 03:01+0000\n"
 "Last-Translator: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "Language-Team: ShaoHe Feng <shaohef@linux.vnet.ibm.com>\n"
 "MIME-Version: 1.0\n"
@@ -56,11 +56,11 @@ msgstr "Burnet项目"
 msgid "Language:"
 msgstr "语言："
 
-#: ui/pages/burnet-ui.html.tmpl:86 ui/pages/dev-ui.html.tmpl:75
+#: ui/pages/burnet-ui.html.tmpl:87 ui/pages/dev-ui.html.tmpl:75
 msgid "Guests"
 msgstr "客户机"
 
-#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guest.html.tmpl:47
+#: ui/pages/dev-ui.html.tmpl:76 ui/pages/guestTab.html.tmpl:49
 msgid "Storage"
 msgstr "存储"
 
@@ -97,7 +97,7 @@ msgid "Settings"
 msgstr "设置"
 
 #: ui/pages/dev-ui.html.tmpl:94 ui/pages/dev-ui.html.tmpl:105
-#: ui/pages/guest.html.tmpl:85 ui/pages/template.html.tmpl:61
+#: ui/pages/guest.html.tmpl:68 ui/pages/template.html.tmpl:61
 msgid "Delete"
 msgstr "删除"
 
@@ -105,7 +105,7 @@ msgstr "删除"
 msgid "More..."
 msgstr "更多"
 
-#: ui/pages/burnet-ui.html.tmpl:89 ui/pages/dev-ui.html.tmpl:102
+#: ui/pages/burnet-ui.html.tmpl:90 ui/pages/dev-ui.html.tmpl:102
 #: ui/pages/template.html.tmpl:48
 msgid "Templates"
 msgstr "模版"
@@ -134,24 +134,24 @@ msgstr "推特"
 msgid "IRC"
 msgstr "IRC"
 
-#: ui/pages/guest.html.tmpl:44
+#: ui/pages/guestTab.html.tmpl:46
 msgid "Name"
 msgstr "名称"
 
-#: ui/pages/guest.html.tmpl:45 ui/pages/template-edit.html.tmpl:71
+#: ui/pages/guestTab.html.tmpl:47 ui/pages/template-edit.html.tmpl:71
 msgid "CPU"
 msgstr "处理器"
 
-#: ui/pages/guest-add.html.tmpl:64 ui/pages/guest.html.tmpl:46
-#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:75
+#: ui/pages/guest-add.html.tmpl:64 ui/pages/guestTab.html.tmpl:48
+#: ui/pages/template-edit.html.tmpl:79 ui/pages/template.html.tmpl:78
 msgid "Memory"
 msgstr "内存"
 
-#: ui/pages/guest.html.tmpl:48
+#: ui/pages/guestTab.html.tmpl:50
 msgid "Livetile"
 msgstr "屏幕"
 
-#: ui/pages/guest.html.tmpl:49 ui/pages/guest.html.tmpl:82
+#: ui/pages/guest.html.tmpl:65 ui/pages/guestTab.html.tmpl:51
 #: ui/pages/template.html.tmpl:58
 msgid "Actions"
 msgstr "操作"
@@ -160,7 +160,7 @@ msgstr "操作"
 msgid "Create a New Virtual Machine"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:35 ui/pages/template-add.html.tmpl:35
+#: ui/pages/guest-add.html.tmpl:35
 msgid "Virtual Machine Name"
 msgstr ""
 
@@ -178,7 +178,7 @@ msgstr ""
 msgid "Please choose a template."
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:69
+#: ui/pages/guest-add.html.tmpl:61 ui/pages/template.html.tmpl:72
 msgid "OS"
 msgstr ""
 
@@ -190,8 +190,8 @@ msgstr ""
 msgid "CPUS"
 msgstr ""
 
-#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:79
-#: ui/pages/template-add.html.tmpl:90
+#: ui/pages/guest-add.html.tmpl:76 ui/pages/template-add.html.tmpl:78
+#: ui/pages/template-add.html.tmpl:89
 msgid "Create"
 msgstr ""
 
@@ -203,47 +203,47 @@ msgstr ""
 msgid "Edit"
 msgstr ""
 
-#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:70
+#: ui/pages/template-edit.html.tmpl:63 ui/pages/template.html.tmpl:73
 msgid "Version"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:73
+#: ui/pages/template.html.tmpl:76
 msgid "Default Settings"
 msgstr ""
 
-#: ui/pages/template.html.tmpl:74
+#: ui/pages/template.html.tmpl:77
 msgid "CPUs"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:41
+#: ui/pages/template-add.html.tmpl:40
 msgid "Add Template"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:49
+#: ui/pages/template-add.html.tmpl:48
 msgid "What would you like to do?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:53
+#: ui/pages/template-add.html.tmpl:52
 msgid "Select a specific ISO to create a template."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:60
+#: ui/pages/template-add.html.tmpl:59
 msgid "Where is the ISO file?"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:64
+#: ui/pages/template-add.html.tmpl:63
 msgid "The ISO is on my server."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:68
+#: ui/pages/template-add.html.tmpl:67
 msgid "I want to stream an ISO from the Internet."
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:75
+#: ui/pages/template-add.html.tmpl:74
 msgid "Select the ISO you\\342\\200\\231d like to use to create a template:"
 msgstr ""
 
-#: ui/pages/template-add.html.tmpl:86
+#: ui/pages/template-add.html.tmpl:85
 msgid "Please enter a URL:"
 msgstr ""
 
@@ -306,3 +306,39 @@ msgstr ""
 #: ui/pages/template-edit.html.tmpl:138
 msgid "Save"
 msgstr ""
+
+#: ui/pages/i18n.html.tmpl:40
+msgid "temp.msg.fail.list"
+msgstr "展示模板列表失败"
+
+#: ui/pages/i18n.html.tmpl:41
+msgid "temp.msg.choose.temp"
+msgstr "请选择模板"
+
+#: ui/pages/i18n.html.tmpl:42
+msgid "vm.msg.fail.create.vm"
+msgstr "新建虚拟机失败"
+
+#: ui/pages/i18n.html.tmpl:43
+msgid "msg.fail.start"
+msgstr "启动失败"
+
+#: ui/pages/i18n.html.tmpl:44
+msgid "msg.fail.stop"
+msgstr "停止失败"
+
+#: ui/pages/i18n.html.tmpl:45
+msgid "msg.fail.reset"
+msgstr "重置失败"
+
+#: ui/pages/i18n.html.tmpl:46
+msgid "msg.fail.delete"
+msgstr "删除失败"
+
+#: ui/pages/i18n.html.tmpl:47
+msgid "msg.fail.list.guests"
+msgstr "展示客户机失败"
+
+#: ui/pages/i18n.html.tmpl:48
+msgid "msg.fail.create.template"
+msgstr "新建模板失败"

ui/js/src/burnet.guest_add_main.js
@@ -29,21 +29,21 @@ burnet.guest_add_main = function() {
 			$('#templateTile').html(html);
 		}
 	},function() {
-		burnet.message.error('Failed to list templates');
+		burnet.message.error(i18n['temp.msg.fail.list']);
 	});
 
 	$('#vm-doAdd').on('click', function(event) {
 		var formData = $('#form-vm-add').serializeObject();
 
 		if(!formData.template) {
-			burnet.message.warn('Please choose a template!');
+			burnet.message.warn(i18n['temp.msg.choose.temp']);
 			return;
 		}
 		burnet.createVM(formData,function() {
 			burnet.listVmsAuto();
 			burnet.window.close();
 		},function() {
-			burnet.message.error('Failed to create vm');
+			burnet.message.error(i18n['vm.msg.fail.create.vm']);
 		});
 	});
 };

ui/js/src/burnet.guest_main.js
@@ -26,7 +26,7 @@ burnet.initVmButtonsAction = function() {
 			burnet.startVM($(this).data('vm'), function(result) {
 				burnet.listVmsAuto();
 			},function() {
-				burnet.message.error('Failed to start');
+				burnet.message.error(i18n['msg.fail.start']);
 			});
 		}
 		else {
@@ -42,7 +42,7 @@ burnet.initVmButtonsAction = function() {
 			burnet.stopVM($(this).data('vm'), function(result) {
 				burnet.listVmsAuto();
 			},function() {
-				burnet.message.error('Failed to stop');
+				burnet.message.error(i18n['msg.fail.stop']);
 			});
 		}
 		else {
@@ -83,13 +83,13 @@ burnet.initVmButtonsAction = function() {
 			burnet.resetVM($(this).data('vm'), function(result) {
 				burnet.listVmsAuto();
 			},function() {
-				burnet.message.error('Failed to reset');
+				burnet.message.error(i18n['msg.fail.reset']);
 			});
 		} else {
 			burnet.startVM($(this).data('vm'), function(result) {
 				burnet.listVmsAuto();
 			},function() {
-				burnet.message.error('Failed to start');
+				burnet.message.error(i18n['msg.fail.start']);
 			});
 		}
 	});
@@ -98,7 +98,7 @@ burnet.initVmButtonsAction = function() {
 		burnet.deleteVM($(this).data('vm'), function(result) {
 			burnet.listVmsAuto();
 		},function() {
-			burnet.message.error('Failed to delete');
+			burnet.message.error(i18n['msg.fail.delete']);
 		});
 	});
 
@@ -153,7 +153,7 @@ burnet.listVmsAuto = function() {
 			burnet.initVmButtonsAction();
 		}
 	},function() {
-		burnet.message.error('Failed to list guests');
+		burnet.message.error(i18n['msg.fail.list.guests']);
 	});
 	burnet.vmTimeout = window.setTimeout("burnet.listVmsAuto();", 5000);
 };

ui/js/src/burnet.template_add_main.js
@@ -73,7 +73,7 @@ burnet.template_add_main = function() {
 			burnet.doListTemplates();
 			burnet.window.close();
 		}, function() {
-			burnet.message.error('Failed to create template');
+			burnet.message.error(i18n['msg.fail.create.template']);
 		});
 	});
 
@@ -86,7 +86,7 @@ burnet.template_add_main = function() {
 			burnet.doListTemplates();
 			burnet.window.close();
 		}, function() {
-			burnet.message.error('Failed to create template');
+			burnet.message.error(i18n['msg.fail.create.template']);
 		});
 	});
 

ui/pages/i18n.html.tmpl
@@ -1,0 +1,51 @@
+#*
+ * Project Burnet
+ *
+ * Copyright IBM, Corp. 2013
+ *
+ * Authors:
+ *  Mei Na Zhou <zhoumein@linux.vnet.ibm.com>
+ *  ShaoHe Feng <shaohef@linux.vnet.ibm.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *#
+#unicode UTF-8
+#import gettext
+#silent t = gettext.translation($lang.domain, $lang.localedir, languages=$lang.lang)
+#silent _ = t.gettext
+#silent _t = t.gettext
+
+#set $q = "'"
+#set $dq = '"'
+<!DOCTYPE html>
+<html class="no-js" lang=$lang.lang[0]>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>language</title>
+</head>
+<body>
+<script>
+var i18n = {
+    'temp.msg.fail.list': $dq$_('temp.msg.fail.list')$dq,
+    'temp.msg.choose.temp': $dq$_('temp.msg.choose.temp')$dq,
+    'vm.msg.fail.create.vm': $dq$_('vm.msg.fail.create.vm')$dq,
+    'msg.fail.start': $dq$_('msg.fail.start')$dq,
+    'msg.fail.stop': $dq$_('msg.fail.stop')$dq,
+    'msg.fail.reset': $dq$_('msg.fail.reset')$dq,
+    'msg.fail.delete': $dq$_('msg.fail.delete')$dq,
+    'msg.fail.list.guests': $dq$_('msg.fail.list.guests')$dq,
+    'msg.fail.create.template': $dq$_('msg.fail.create.template')$dq};
+</script>
+</body>
+</html>

whimsy-cat/cherrypy-wok

commit 32672e1054fa0980ee59b5949790f01edf00a264

ui: Ensure i18n.html loads
Commit cc89b1e3eb53b2e05813e6e4fe1e5ceee517b8bf introduced a bug where
the guest list would not load properly when the application is first
loaded.  This is because the i18n.html may not have been loaded and
inserted into the DOM before guestTab.html was loaded.  If this happens,
the DOM will contain errors and not load correctly.  Use the .load()
completion callback to ensure that i18n.html is loaded before
proceeding.

Fixes https://github.com/kimchi-project/kimchi/issues/73

Signed-off-by: Adam Litke <agl@linux.vnet.ibm.com>
Message-id: 1374861991-17172-1-git-send-email-agl@linux.vnet.ibm.com

ui/js/src/burnet.main.js
@@ -38,6 +38,7 @@ burnet.main = function() {
 	var width = currentMenu.parent().width();
 	$('.menu-arrow').css('left', left+width/2-10);
 
-	$('#main').load('i18n.html');
-	$('#main').load('guestTab.html');
+	$('#main').load('i18n.html', function() {
+	    $(this).load('guestTab.html');
+	});
 };